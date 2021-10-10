c90421b02e414998d60e2fb454c8cd373a367ce3
Sigit Budiarmawan <cigetbudi@outlook.com>
2021-10-10 23:35:14 +0700

    Sec 3 - Setting AutoMapper

--- ParkyAPI/Models/NationalPark.cs
+++ ParkyAPI/Models/NationalPark.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ParkyAPI.Models
 {
-    public class NationalParkDTO
+    public class NationalPark
     {
         [Key]
         public int Id { get; set; }
