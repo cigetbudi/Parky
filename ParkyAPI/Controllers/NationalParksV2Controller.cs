ff75b52904407ba0341494f6ed9d9ecff99f51ac
Sigit Budiarmawan <cigetbudi@outlook.com>
2021-10-12 21:18:28 +0700

    Sec 7 - API Versioning Part 1

---
+++ ParkyAPI/Controllers/NationalParksV2Controller.cs
@@ -0,0 +1,49 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ParkyAPI.Models;
+using ParkyAPI.Models.DTOs;
+using ParkyAPI.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParkyAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    //[ApiExplorerSettings(GroupName = "ParkyOpenAPISpecNP")]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public class NationalParksV2Controller : ControllerBase
+    {
+        private INationalParkRepository _npRepo;
+        private readonly IMapper _mapper;
+
+        public NationalParksV2Controller(INationalParkRepository npRepo, IMapper mapper)
+        {
+            _npRepo = npRepo;
+            _mapper = mapper;
+        }
+
+
+        /// <summary>
+        /// Ambil semua list National Parks
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [ProducesResponseType(200, Type =typeof(List<NationalParkDTO>))]
+        public IActionResult GetNationalParks()
+        {
+            var objList = _npRepo.GetNationalParks();
+            var objDTO = new List<NationalParkDTO>();
+
+            foreach (var obj in objList)
+            {
+                objDTO.Add(_mapper.Map<NationalParkDTO>(obj));
+            }
+            return Ok(objDTO);
+        }
+
+    }
+}
--- ParkyAPI/Startup.cs
+++ ParkyAPI/Startup.cs
@@ -37,33 +37,22 @@ namespace ParkyAPI
             services.AddScoped<INationalParkRepository, NationalParkRepository>();
             services.AddScoped<ITrailRepository, TrailRepository>();
             services.AddAutoMapper(typeof(ParkyMappings));
-            services.AddSwaggerGen(options => {
-                //versioning
-                options.SwaggerDoc("ParkyOpenAPISpecNP",
-                    new Microsoft.OpenApi.Models.OpenApiInfo()
-                    {
-                        Title = "Parky API (National Park)",
-                        Version = "1",
-                        Description = "Belajar Parky API NP",
-                        Contact= new Microsoft.OpenApi.Models.OpenApiContact()
-                        {
-                            Email = "cigetbudi@gmail.com",
-                            Name = "Sigit Budi Darmawan",
-                            Url = new Uri("https://www.linkedin.com/in/cigetbudi")
-                        },
-                        License = new Microsoft.OpenApi.Models.OpenApiLicense()
-                        {
-                            Name = "MIT License",
-                            Url = new Uri("https://en.wikipedia.org/wiki/MIT_License")
-                        }
-                    });
+            services.AddApiVersioning(options =>
+                {
+                    options.AssumeDefaultVersionWhenUnspecified = true;
+                    options.DefaultApiVersion = new ApiVersion(1, 0);
+                    options.ReportApiVersions = true;
+                });
 
-                options.SwaggerDoc("ParkyOpenAPISpecTrails",
+            services.AddSwaggerGen(options =>
+            {
+                //versioning
+                options.SwaggerDoc("ParkyOpenAPISpec",
                     new Microsoft.OpenApi.Models.OpenApiInfo()
                     {
-                        Title = "Parky API (Trail)",
+                        Title = "Parky API",
                         Version = "1",
-                        Description = "Belajar Parky API Trail",
+                        Description = "Belajar Parky API",
                         Contact = new Microsoft.OpenApi.Models.OpenApiContact()
                         {
                             Email = "cigetbudi@gmail.com",
@@ -76,6 +65,25 @@ namespace ParkyAPI
                             Url = new Uri("https://en.wikipedia.org/wiki/MIT_License")
                         }
                     });
+
+                //options.SwaggerDoc("ParkyOpenAPISpecTrails",
+                //    new Microsoft.OpenApi.Models.OpenApiInfo()
+                //    {
+                //        Title = "Parky API (Trail)",
+                //        Version = "1",
+                //        Description = "Belajar Parky API Trail",
+                //        Contact = new Microsoft.OpenApi.Models.OpenApiContact()
+                //        {
+                //            Email = "cigetbudi@gmail.com",
+                //            Name = "Sigit Budi Darmawan",
+                //            Url = new Uri("https://www.linkedin.com/in/cigetbudi")
+                //        },
+                //        License = new Microsoft.OpenApi.Models.OpenApiLicense()
+                //        {
+                //            Name = "MIT License",
+                //            Url = new Uri("https://en.wikipedia.org/wiki/MIT_License")
+                //        }
+                //    });
 
                 var xmlCommentFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
                 var cmlCommentsFullPatch = Path.Combine(AppContext.BaseDirectory, xmlCommentFile);
@@ -97,8 +105,8 @@ namespace ParkyAPI
             app.UseSwagger();
             app.UseSwaggerUI(options =>
             {
-                options.SwaggerEndpoint("/swagger/ParkyOpenAPISpecNP/swagger.json", "Parky API NP");
-                options.SwaggerEndpoint("/swagger/ParkyOpenAPISpecTrails/swagger.json", "Parky API Trails");
+                options.SwaggerEndpoint("/swagger/ParkyOpenAPISpec/swagger.json", "Parky API");
+                //options.SwaggerEndpoint("/swagger/ParkyOpenAPISpecTrails/swagger.json", "Parky API Trails");
                 options.RoutePrefix = "";
             });
             app.UseRouting();
