f298f0d24d91ef86b5a10b1bc5a35c777eab1083
Sigit Budiarmawan <cigetbudi@outlook.com>
2021-10-13 22:21:49 +0700

    Sec 7 - API Versioning Part 2

--- ParkyAPI/Startup.cs
+++ ParkyAPI/Startup.cs
@@ -2,15 +2,18 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using ParkyAPI.Data;
 using ParkyAPI.ParkyMapper;
 using ParkyAPI.Repository;
 using ParkyAPI.Repository.IRepository;
+using Swashbuckle.AspNetCore.SwaggerGen;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -43,57 +46,60 @@ namespace ParkyAPI
                     options.DefaultApiVersion = new ApiVersion(1, 0);
                     options.ReportApiVersions = true;
                 });
+            services.AddVersionedApiExplorer(options => options.GroupNameFormat = "'v'VVV");
+            services.AddSwaggerGen();
+            services.AddTransient<IConfigureOptions<SwaggerGenOptions>, ConfigureSwaggerOptions>();
 
-            services.AddSwaggerGen(options =>
-            {
-                //versioning
-                options.SwaggerDoc("ParkyOpenAPISpec",
-                    new Microsoft.OpenApi.Models.OpenApiInfo()
-                    {
-                        Title = "Parky API",
-                        Version = "1",
-                        Description = "Belajar Parky API",
-                        Contact = new Microsoft.OpenApi.Models.OpenApiContact()
-                        {
-                            Email = "cigetbudi@gmail.com",
-                            Name = "Sigit Budi Darmawan",
-                            Url = new Uri("https://www.linkedin.com/in/cigetbudi")
-                        },
-                        License = new Microsoft.OpenApi.Models.OpenApiLicense()
-                        {
-                            Name = "MIT License",
-                            Url = new Uri("https://en.wikipedia.org/wiki/MIT_License")
-                        }
-                    });
+            //services.AddSwaggerGen(options =>
+            //{
+            //    //versioning
+            //    options.SwaggerDoc("ParkyOpenAPISpec",
+            //        new Microsoft.OpenApi.Models.OpenApiInfo()
+            //        {
+            //            Title = "Parky API",
+            //            Version = "1",
+            //            Description = "Belajar Parky API",
+            //            Contact = new Microsoft.OpenApi.Models.OpenApiContact()
+            //            {
+            //                Email = "cigetbudi@gmail.com",
+            //                Name = "Sigit Budi Darmawan",
+            //                Url = new Uri("https://www.linkedin.com/in/cigetbudi")
+            //            },
+            //            License = new Microsoft.OpenApi.Models.OpenApiLicense()
+            //            {
+            //                Name = "MIT License",
+            //                Url = new Uri("https://en.wikipedia.org/wiki/MIT_License")
+            //            }
+            //        });
 
-                //options.SwaggerDoc("ParkyOpenAPISpecTrails",
-                //    new Microsoft.OpenApi.Models.OpenApiInfo()
-                //    {
-                //        Title = "Parky API (Trail)",
-                //        Version = "1",
-                //        Description = "Belajar Parky API Trail",
-                //        Contact = new Microsoft.OpenApi.Models.OpenApiContact()
-                //        {
-                //            Email = "cigetbudi@gmail.com",
-                //            Name = "Sigit Budi Darmawan",
-                //            Url = new Uri("https://www.linkedin.com/in/cigetbudi")
-                //        },
-                //        License = new Microsoft.OpenApi.Models.OpenApiLicense()
-                //        {
-                //            Name = "MIT License",
-                //            Url = new Uri("https://en.wikipedia.org/wiki/MIT_License")
-                //        }
-                //    });
+            //    //options.SwaggerDoc("ParkyOpenAPISpecTrails",
+            //    //    new Microsoft.OpenApi.Models.OpenApiInfo()
+            //    //    {
+            //    //        Title = "Parky API (Trail)",
+            //    //        Version = "1",
+            //    //        Description = "Belajar Parky API Trail",
+            //    //        Contact = new Microsoft.OpenApi.Models.OpenApiContact()
+            //    //        {
+            //    //            Email = "cigetbudi@gmail.com",
+            //    //            Name = "Sigit Budi Darmawan",
+            //    //            Url = new Uri("https://www.linkedin.com/in/cigetbudi")
+            //    //        },
+            //    //        License = new Microsoft.OpenApi.Models.OpenApiLicense()
+            //    //        {
+            //    //            Name = "MIT License",
+            //    //            Url = new Uri("https://en.wikipedia.org/wiki/MIT_License")
+            //    //        }
+            //    //    });
 
-                var xmlCommentFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-                var cmlCommentsFullPatch = Path.Combine(AppContext.BaseDirectory, xmlCommentFile);
-                options.IncludeXmlComments(cmlCommentsFullPatch);
-            });
+            //    var xmlCommentFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+            //    var cmlCommentsFullPatch = Path.Combine(AppContext.BaseDirectory, xmlCommentFile);
+            //    options.IncludeXmlComments(cmlCommentsFullPatch);
+            //});
             services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IApiVersionDescriptionProvider provider)
         {
             if (env.IsDevelopment())
             {
