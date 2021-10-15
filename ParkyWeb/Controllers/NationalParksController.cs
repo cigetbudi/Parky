68d805b93a54deb448af37f462b72acd09f273c8
Sigit Budiarmawan <cigetbudi@outlook.com>
2021-10-15 21:59:42 +0700

    Sec 9 - Upsert GET Action Method

--- ParkyWeb/Controllers/NationalParksController.cs
+++ ParkyWeb/Controllers/NationalParksController.cs
@@ -20,6 +20,26 @@ namespace ParkyWeb.Controllers
             return View(new NationalPark() { });
         }
 
+        //GET CREATE/UPDATE
+        public async Task<IActionResult> Upsert(int? id)
+        {
+            NationalPark obj = new NationalPark();
+            if (id == null)
+            {
+                //create nampilin National Park kosongan as obj
+                return View(obj);
+            }
+            //get update by id
+            obj = await _npRepo.GetAsync(SD.NationalParkAPIPath, id.GetValueOrDefault());
+            if (obj == null)
+            {
+                //kalo id ga ada
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+
         public async Task<IActionResult> GetAllNationalPark()
         {
             return Json(new { data = await _npRepo.GetAllAsync(SD.NationalParkAPIPath)});
