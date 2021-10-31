f9c800f1c8ce0e86f9acd0dd87ad461583ed1df1
Sigit Budiarmawan <cigetbudi@outlook.com>
2021-10-31 22:20:16 +0700

    Sec 12 - Add Authenticate method

---
+++ ParkyAPI/Controllers/UsersController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ParkyAPI.Models;
+using ParkyAPI.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParkyAPI.Controllers
+{
+    [Authorize]
+    [Route("api/v{version:apiVersion}/Users")]
+    //[Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserRepository _uRepo;
+        public UsersController(IUserRepository uRepo)
+        {
+            _uRepo = uRepo;
+        }
+
+        public IActionResult Authenticate([FromBody] User model)
+        {
+            var user = _uRepo.Authenticate(model.Username, model.Password);
+            if (user == null)
+            {
+                return BadRequest(new { message = "Username atau password tidak sesuai!" });
+            }
+            return Ok(user);
+        }
+    }
+}
